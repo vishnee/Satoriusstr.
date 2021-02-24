588307847931de843a0e62bb9ac09efe78ee222e
vishnee <vishnee@vishnee.de>
2021-02-24 22:41:49 +0100

    added tilesets

---
+++ Flat_walls_floors_32x32_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Flat_walls_floors_32x32_1" tilewidth="32" tileheight="32" tilecount="140" columns="10">
+ <image source="tiles/Flat_walls_floors_32x32_1.png" width="320" height="448"/>
+</tileset>
---
+++ Office_walls_floors_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Office_walls_floors_32x32" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tiles/Office_walls_floors_32x32.png" width="512" height="384"/>
+</tileset>

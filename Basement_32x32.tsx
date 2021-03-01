ec853c4d79ccd08eeb6e8eba3d258f031fbe2508
vishnee <45599825+vishnee@users.noreply.github.com>
2021-03-01 20:36:33 +0000

    Deploying to gh-pages from master @ eeae4c60b779ef8b7ef1ab18e9aa121dbd96a13c 🚀

--- Basement_32x32.tsx
+++ Basement_32x32.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Basement_32x32" tilewidth="32" tileheight="32" tilecount="800" columns="16">
  <image source="tiles/Basement_32x32.png" width="512" height="1600"/>
+ <tile id="508">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="4.21053" y="31.5789">
+    <polygon points="0,0 -0.263158,-15.2632 27.3684,-15.3947 27.6316,0.394737"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
